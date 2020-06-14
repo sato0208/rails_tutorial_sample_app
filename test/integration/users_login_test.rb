972e01b881681fa1d222dff9cb2212c4a5566738
sato chiharu <kujira028@gmail.com>
2020-06-14 09:05:01 +0000

    [update]ログイン認証

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     # ログイン用のパスを開く
     get login_path
@@ -14,5 +18,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     # 移動先のページでフラッシュメッセージが表示されていないことを確認
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    # リダイレクト先が正しいかどうかをチェック
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,5 +27,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     # フラッシュメッセージが空でないか検証する
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
