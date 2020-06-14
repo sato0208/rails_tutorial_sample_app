22cfb863fbf7c55a561320d6dae77e0e0d1a47b2
sato chiharu <kujira028@gmail.com>
2020-06-14 06:56:42 +0000

    [update]ユーザーをデータベースから見つけて検証

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+      # 送信されたメールアドレスを使って、データベースからユーザーを取り出す
+    user = User.find_by(email: params[:session][:email].downcase)
+      # ユーザーがデータベースにあり、かつ、認証に成功した場合にのみ
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+    end
+  end
+
+
+  def destroy
   end
 end
