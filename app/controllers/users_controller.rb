67d80dc79f7ea529f5fc7eb2730fdff6df0bdc73
sato chiharu <kujira028@gmail.com>
2020-06-16 13:20:28 +0000

    [add]gem 'faker'追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   # ユーザーにログインを要求する
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :index]
+  # 編集/更新ページを保護
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
@@ -47,8 +53,15 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -19,20 +19,26 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               assert_select "div.alert", "The form contains 4 errors."
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
+    # edit_user_path(@user)にgetのリクエスト
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    # session[:forwarding_url]とedit_user_url(@user)が等しい時にtrue
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    # @userとしてログイン
+    log_in_as(@user)
+    # edit用のテンプレートはリダイレクトで描画されるので下記一文は削除
+    # assert_template 'users/edit'
+    # session[:forwarding_url]がnilの時true
+    assert_nil session[:forwarding_url]
+    # @userのユーザー編集ページにリダイレクトされる
+    assert_redirected_to edit_user_url(@user)
+    # nameに"Foo Bar"を代入
     name  = "Foo Bar"
+    # emailに"foo@bar.com"を代入
     email = "foo@bar.com"
+    # 有効なparams:を持ったuser_path(@user)でpatch（更新）のリクエスト
     patch user_path(@user), params: { user: { name:  name,
                                               email: email,
                                               password:              "",
                                               password_confirmation: "" } }
-    assert_not flash.empty?
-    assert_redirected_to @user
-    @user.reload
-    assert_equal name,  @user.name
-    assert_equal email, @user.email
-  end
-end
+・
