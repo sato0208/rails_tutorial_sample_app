aa4f46f625f6a8d57e0ef6a7b979f0fbdc152c69
sato chiharu <kujira028@gmail.com>
2020-06-18 03:23:12 +0000

    [update]サンプルユーザーの生成とテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  # アカウント有効化のコードを追加
+  attr_accessor :remember_token, :activation_token
 
   # email属性を小文字に変換してメールアドレスの一意性を保証
-  before_save { email.downcase! }
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   # メールフォーマットを正規表現で検証
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -48,4 +50,16 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+  private
+
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email.downcase!
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
