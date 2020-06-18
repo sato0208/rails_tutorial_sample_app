571e287956ac79db5599077cd424bd733eda18e2
sato chiharu <kujira028@gmail.com>
2020-06-18 04:51:07 +0000

    [update]送信メールのテスト

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,13 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # アカウント有効化のプレビューメソッド (完成)
   def account_activation
-    UserMailer.account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
