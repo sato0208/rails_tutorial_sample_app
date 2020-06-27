465e4effa5ff3893e655c716a7d4aa358d4bb0d6
sato chiharu <kujira028@gmail.com>
2020-06-27 10:02:27 +0000

    [update]エラー表示部分テンプレート共通化のための修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    # @micropostに　ユーザーがログインしていればログイン中のユーザーに紐付いたMicropostオブジェクトを返す
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
