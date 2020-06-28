c1acbc701e0ab1536816b42e8a9c6175548ae9f8
sato chiharu <kujira028@gmail.com>
2020-06-28 07:29:04 +0000

    [update]画像アップロード機能のための設定

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
   # default_scope（順序を指定するメソッド） created_at:を降順にする
   default_scope -> { order(created_at: :desc) }
+  # Micropostモデルに画像を追加
+  mount_uploader :picture, PictureUploader
   # user_idが存在する
   validates :user_id, presence: true
   # Micropostモデルのバリデーション
