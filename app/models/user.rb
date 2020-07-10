2c8f7da23bbebc7dcf5bed706ceeb7f5b9203e9e
sato chiharu <kujira028@gmail.com>
2020-07-10 14:07:01 +0000

    [update]following 関連の機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
   foreign_key: "follower_id",
   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   # アカウント有効化のコードを追加
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -89,6 +90,21 @@ class User < ApplicationRecord
   def feed
     # Micropostテーブルからuser_idがidのユーザーをすべて取得
     Micropost.where("user_id = ?", id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
