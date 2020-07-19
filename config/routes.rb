6e50ed20c3476a499daba94889e064a051c9db2b
sato chiharu <kujira028@gmail.com>
2020-07-19 07:41:51 +0000

    [update]Relationshipリソース用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ end
 resources :account_activations, only: [:edit]
 resources :password_resets,     only: [:new, :create, :edit, :update]
 resources :microposts,          only: [:create, :destroy]
+resources :relationships,       only: [:create, :destroy]
 end
