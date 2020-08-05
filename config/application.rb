38873e6bf073e4d7d03f5ef499a7cd93de96c636
sato chiharu <kujira028@gmail.com>
2020-08-05 13:03:11 +0000

    config/application.rbにファイル読み込みのコードを追加

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module SampleApp
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
